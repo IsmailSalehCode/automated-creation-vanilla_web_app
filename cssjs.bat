2c6837557ef3b34ce16045ba1ba9cdca74e19915
Ismail Saleh <centriu78@gmail.com>
2022-12-30 21:42:18 +0200

    h

--- cssjs.bat
+++ cssjs.bat
@@ -1,4 +1,4 @@
-ECHO OFF 
+@ECHO OFF 
 SET templName=vanilla_project
 @REM  %templName% - variable, referencing the name of the folder, containing the template files
 
@@ -6,7 +6,7 @@ SET source = %~dp0%templName%
 @REM  %~dp0 - environment arguments, forming the full path of the folder in which this bat file resides. For more information: https://stackoverflow.com/a/112135/16882316
 @REM  %source% - variable, storing the source path of the template items we need to transfer. The variable is formed by concatenating the values of %templName% and %~dp0
 
-set /p "newFolderName=Enter name for the new project: "
+SET /p "newFolderName=Enter name for the new project: "
 @REM  Prompt user for setting the value of %newFolderName%, which will be the designated directory for the transfer of files.
 
 md %newFolderName%
@@ -18,7 +18,24 @@ SET destination= %cd%\%newFolderName%
 robocopy %~dp0%templName% %destination% /E
 :: robocopy - function, which copies file data from one location to another. For more information: https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/robocopy
 
-start %destination% .c
+:choice
+SET /p choice=Do you want to open it with VSCode? [Y/n]:
+IF NOT '%choice%'=='' SET choice=%choice:~0,1%
+IF '%choice%'=='Y' GOTO yes
+IF '%choice%'=='y' GOTO yes
+IF '%choice%'=='N' GOTO no
+IF '%choice%'=='n' GOTO no
+IF '%choice%'=='' GOTO yes
+echo Not found.
+GOTO commonexit
 
-cmd /c
-:: Terminate cmd 
+:no
+ECHO bye!
+GOTO commonexit
+
+:yes
+start code %destination%
+@REM Open with VSCode
+GOTO commonexit
+
+:commonexit
